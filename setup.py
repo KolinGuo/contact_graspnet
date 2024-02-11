e4e48906fea78fa6fb99cc40c9a9172496df8c1c
Kolin Guo <ruguo@ucsd.edu>
2024-02-11 14:16:22 -0800

    Move gripper_control_points & gripper_models into contact_graspnet/

--- setup.py
+++ setup.py
@@ -4,8 +4,15 @@ if __name__ == "__main__":
     setup(
         name="contact_graspnet",
         packages=find_packages(include="contact_graspnet*"),
+        package_dir={"contact_graspnet": "contact_graspnet"},
         package_data={
             "contact_graspnet.pointnet2.tf_ops": ["**/*.so"],
             "contact_graspnet": ["config.yaml"],
+            "contact_graspnet.gripper_control_points": [
+                "panda.npy",
+                "panda_gripper_coords.pickle",
+                "panda_gripper_coords.yml",
+            ],
+            "contact_graspnet.gripper_models": ["**/*"],
         },
     )
