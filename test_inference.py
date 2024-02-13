0112c3a01a57baa02dcc82a3f13b695f6cd08b43
Kolin Guo <ruguo@ucsd.edu>
2024-02-13 14:41:37 -0800

    Add main() for easy copy

--- test_inference.py
+++ test_inference.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 import numpy as np
 from real_robot.utils.logger import get_logger
+from real_robot.utils.visualization import Visualizer
 
 from contact_graspnet import CGNGraspEstimator
+# ruff: noqa: F841
 
 
 def download_test_data(output_dir: Path) -> None:
@@ -18,6 +20,30 @@ def download_test_data(output_dir: Path) -> None:
     if ret is None:
         raise RuntimeError("Failed to download test data")
     logger.info("Finish downloading test data...")
+
+
+def main(data, args) -> None:
+    grasp_estimator = CGNGraspEstimator(
+        "hor_sigma_0025", forward_passes=args.forward_passes, gripper_type="panda"
+    )
+    pred_grasps_cam, scores, contact_pts, q_vals = grasp_estimator.predict_grasps(
+        depth=data["depth"],
+        seg=data["seg"],
+        K=data["K"],
+        local_regions=args.no_local_regions,
+        filter_grasps=args.no_filter_grasps,
+        seg_id=args.seg_id,
+        z_clip_range=args.z_range,
+        skip_border_objects=args.skip_border_objects,
+        margin_px=args.margin_px,
+    )
+    vis: Visualizer = grasp_estimator.visualize_grasps(
+        pred_grasps_cam,
+        scores,
+        q_vals,
+        cam_pose=np.eye(4),
+        rgb_image=data["rgb"][..., ::-1],  # BGR -> RGB
+    )
 
 
 if __name__ == "__main__":
@@ -87,24 +113,4 @@ if __name__ == "__main__":
 
     data = np.load(np_path, allow_pickle=True).item()
 
-    grasp_estimator = CGNGraspEstimator(
-        "hor_sigma_0025", forward_passes=args.forward_passes, gripper_type="panda"
-    )
-    pred_grasps_cam, scores, contact_pts, q_vals = grasp_estimator.predict_grasps(
-        depth=data["depth"],
-        seg=data["seg"],
-        K=data["K"],
-        local_regions=args.no_local_regions,
-        filter_grasps=args.no_filter_grasps,
-        seg_id=args.seg_id,
-        z_clip_range=args.z_range,
-        skip_border_objects=args.skip_border_objects,
-        margin_px=args.margin_px,
-    )
-    vis = grasp_estimator.visualize_grasps(
-        pred_grasps_cam,
-        scores,
-        q_vals,
-        cam_pose=np.eye(4),
-        rgb_image=data["rgb"][..., ::-1],  # BGR -> RGB
-    )
+    main(data, args)
